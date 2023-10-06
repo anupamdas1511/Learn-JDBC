faa8060ea7cf3f7e5862bfb09cf210babe231e3f
Anupam Das <dasanupam151101@gmail.com>
2023-10-07 00:51:40 +0530

    connection created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/learnJDBC";
+            String username = "root";
+            String password = "password";
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            if (connection.isClosed()) {
+                System.out.println("Connection is closed");
+            } else {
+                System.out.println("Connection created");
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
